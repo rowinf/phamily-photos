ede89c78501ce9b4ae9093c3248a2f660b491bb1
rowinf <964049+rowinf@users.noreply.github.com>
2024-09-18 16:37:32 +1200

    add file server

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/donseba/go-htmx"
 	"github.com/donseba/go-htmx/middleware"
@@ -13,11 +16,6 @@ import (
 type (
 	App struct {
 		htmx *htmx.HTMX
-	}
-
-	route struct {
-		path    string
-		handler http.HandlerFunc
 	}
 )
 
@@ -30,10 +28,15 @@ func main() {
 	mux := chi.NewRouter()
 
 	htmx.UseTemplateCache = false
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "assets", "upload"))
 
 	mux.Use(middleware.MiddleWare)
 	mux.Get("/", app.Home)
 	mux.Get("/child", app.Child)
+	mux.Get("/photo/new", app.PhotoNew)
+	mux.Get("/photo", app.Photo)
+	FileServer(mux, "/uploads", filesDir)
 
 	err := http.ListenAndServe(":3210", mux)
 	log.Fatal(err)
@@ -67,6 +70,51 @@ func (a *App) Child(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Printf("error rendering page: %v", err.Error())
 	}
+}
+
+func (a *App) PhotoNew(w http.ResponseWriter, r *http.Request) {
+	h := a.htmx.NewHandler(w, r)
+	page := htmx.NewComponent("photo-new.html").Wrap(mainContent(), "Content")
+
+	_, err := h.Render(r.Context(), page)
+	if err != nil {
+		fmt.Printf("error rendering page: %v", err.Error())
+	}
+}
+
+func (a *App) Photo(w http.ResponseWriter, r *http.Request) {
+	h := a.htmx.NewHandler(w, r)
+	data := map[string]any{
+		"Title": "Photo Title",
+		"Url":   "/uploads/Lake-Sherwood1.jpg",
+	}
+	page := htmx.NewComponent("photo.html").SetData(data).Wrap(mainContent(), "Content")
+
+	_, err := h.Render(r.Context(), page)
+	if err != nil {
+		fmt.Printf("error rendering page: %v", err.Error())
+	}
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", http.StatusFound).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
 
 func mainContent() htmx.RenderableComponent {
